2e9424be3c7d002da45b79cc3d34bc7b0f1dc31e
Glauter Santos <glauterlima@gmail.com>
2019-05-02 20:56:01 -0300

    funcoes upper() e strip()

--- jogos/forca.py
+++ jogos/forca.py
@@ -10,6 +10,15 @@ def jogar():
 
     #enquanto não enforcou e não acertou
     while(not enforcou and not acertou):
+
+        chute = input("Qual letra?")
+        chute = chute.strip()
+        index = 0
+        for letra in palavra_secreta:
+            if(chute.upper() == letra.upper()):
+                print("Encontrei a letra {} na posição {}".format(letra, index))
+            index = index + 1
+
         print("jogando...")
 
     print("Fim do jogo!")
