45ed88622272912e929a6401dd5da4b6e0e19400
Glauter Santos <glauterlima@gmail.com>
2019-05-02 19:36:12 -0300

    iniciando codificação do jogo forca

--- jogos/forca.py
+++ jogos/forca.py
@@ -3,6 +3,14 @@ def jogar():
     print("Bem vindo ao jogo de forca!")
     print("*********************************")
 
+    palavra_secreta = "banana"
+
+    enforcou = False
+    acertou = False
+
+    #enquanto não enforcou e não acertou
+    while(not enforcou and not acertou):
+        print("jogando...")
 
     print("Fim do jogo!")
 
